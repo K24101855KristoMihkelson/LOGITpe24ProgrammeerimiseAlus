37c6e9119d2db68f2f306c045dacf5495623e285
Opilane TTHK <opilane@tthk.ee>
2025-02-26 11:25:39 +0200

    doublecomma

--- DataTypes/DataTypes/Program.cs
+++ DataTypes/DataTypes/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace DataTypes
+﻿using System.Numerics;
+
+namespace DataTypes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Data types!");
+
+            int bigNumber = 1234567890;
+            Console.WriteLine(bigNumber);
+
+            double comaNumber = 12.5;
+            Console.WriteLine(comaNumber);
         }
-    }
+    }        
 }
