c8f36488bb09e986c962e00c3ce947c862c467e9
Opilane TTHK <opilane@tthk.ee>
2025-02-26 11:39:07 +0200

    Tunnitöö DataTypes,Bool,float,Variables

--- DataTypes/DataTypes/Program.cs
+++ DataTypes/DataTypes/Program.cs
@@ -13,6 +13,9 @@ namespace DataTypes
 
             double comaNumber = 12.5;
             Console.WriteLine(comaNumber);
+
+            string stringVar = "asd53328344397345734575w84537543877567475437547(t3845r3e8te;,sd,s-s.e3-3w.,w,JHG>FXDSLÖMJHZ;KH>LOA(Y mt909t8313´4";
+            Console.WriteLine(stringVar);
         }
     }        
-}
+}           
