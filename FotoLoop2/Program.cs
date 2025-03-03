4b7083953d507d69dcfce4cf93eef27ea1076de6
K24101855KristoMihkelson <krismihkel@gmail.com>
2025-03-03 15:52:25 +0200

    FotoLoop2

---
+++ FotoLoop2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FotoLoop2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+           int arv = Int16.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arv; i++)
+            {
+                Console.WriteLine("See rida on nr {0} ",i);
+                string a = "kill-koll";
+                String b = "killadi-koll";
+
+                for (int j = 1; j < 2; j++) 
+                {
+                    Console.WriteLine(a);
+                }
+
+                for (int k = 0; j <= 1 ki++)
+
+        }
+    }
+}
+
+
