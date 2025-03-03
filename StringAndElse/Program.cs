4c03b4c57a03275aafc933d213d11f340139d9b8
K24101855KristoMihkelson <krismihkel@gmail.com>
2025-03-03 14:12:51 +0200

    Brackets

---
+++ StringAndElse/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace StringAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Sisestage oma nimi";
+
+        }
+    }
+}           string name = Console.ReadLine();
+
+            if (name != "") ;
+       {
+           Console.WriteLine("Sisestasid nime: " + name)
+         
+
+
+
+
+
+
