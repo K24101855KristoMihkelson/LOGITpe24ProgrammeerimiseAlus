0d45bc0160c63ebaa354065683ee6e0e94a2ddcf
K24101855KristoMihkelson <krismihkel@gmail.com>
2025-03-03 15:09:36 +0200

    AroundEarth

---
+++ AroundEarth/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AroundEarth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+            double coinDiameter = 25.75; //mm
+            double earthRadius = 6378000000;
+            double aroundEarth = 2 * Math.PI * earthRadius;
+
+
+            Console.WriteLine("Maa ümbermõõt; " + aroundEarth);
+
+
+            //nüüd alles müntide arv välja arvutada
+            double howManyCoins = aroundEarth / coinDiameter;
+
+
+            Console.WriteLine("Ümber maa mahub nii palju euroseid" + howManyCoins); 
+        }
+    }
+}
+        
