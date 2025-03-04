349e616b7d8e5ba65cc9929ac4f1ca7cdf63c0a1
K24101855KristoMihkelson <krismihkel@gmail.com>
2025-03-04 11:13:51 +0200

    IfElseOddNumbers

---
+++ IfElseOddNumbers/IfElseOddNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace IfElseOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Sisesta number ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (true)
+            {
+                Console.WriteLine("Sisestasid paaris arvu" + number);
+            }
+            else 
+            {
+               Console.WriteLine("Sisetasid paaritu arvu" + number);
+            
+              }
+           }
+    }
+}
