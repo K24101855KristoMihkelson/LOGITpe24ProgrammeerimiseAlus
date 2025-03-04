32c3cee29b183fbc68445df5191c0451877d4010
K24101855KristoMihkelson <krismihkel@gmail.com>
2025-03-04 11:45:35 +0200

    MethodCode2

---
+++ MethodCode2/MethodCode2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MethodCode2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int number = int parse(Console.ReadLine());
+
+            if (int.IsEvenInteger(number))
+            {
+                EvenNumbers(EvenNumbers);
+            }
+            else
+            {
+                OddNumbers(number);
+
+           static void EvenNumbers(int nr) 
+           {
+                Console.WriteLine("Tegemist on paaris arvuga" + nr);
+
+           static void OddNumbers(int nr) 
+           {
+                Console.WriteLine("Tegemist on paaritu arvuga" + nr);
+        }
+    }
+}
