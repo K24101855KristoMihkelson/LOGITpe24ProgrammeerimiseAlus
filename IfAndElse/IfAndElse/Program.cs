22df513cab15ece45ba040cb7a2fdf1059c6b0b9
K24101855KristoMihkelson <krismihkel@gmail.com>
2025-03-04 10:40:39 +0200

    IfAndElse

---
+++ IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Sisesta enda age ");
+
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 0 && age <= 18)
+            {
+                Console.WriteLine("Sa oled alaealine");
+            }
+            else if (age >= 19 && age <= 65)
+            {
+                Console.WriteLine("Sa oled juba täiskasvanud");
+            }
+
+            else
+            {
+                Console.WriteLine("Sa oled pensionäär");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
