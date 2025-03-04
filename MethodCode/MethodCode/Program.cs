c0e920719dfe3362adccdc99bc3044299722e815
K24101855KristoMihkelson <krismihkel@gmail.com>
2025-03-04 11:29:02 +0200

    MethodCode

---
+++ MethodCode/MethodCode/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MethodCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Jalgratas();
+        }
+
+        static void Jalgratas()
+        {
+            Console.WriteLine("Nüüd oled jalgratta meetodis");
+        }
+    }
+}
