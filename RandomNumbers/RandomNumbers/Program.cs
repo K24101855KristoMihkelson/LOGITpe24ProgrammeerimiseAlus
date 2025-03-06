90dfc105c530c7869d10d4e704c4ca98973ab750
Opilane TTHK <opilane@tthk.ee>
2025-03-06 09:57:45 +0200

    RandomColor

---
+++ RandomNumbers/RandomNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Hello, World!");
+int number = new Random().Next(1, 7);
+
+switch (number)
+{
+    case 1:
+        Console.WriteLine("Veeretasin nr " + number);
+        break;
+
+    case 2:
+        Console.WriteLine("Veeretasin arvu nr " + number);
+        break;
+
+    case 3:
+        Console.WriteLine("Veeretasin nr " + number);
+        break;
+
+    case 4:
+        Console.WriteLine("Sisestasin nr " + number);
+        break;
+
+    case 5:
+        Console.WriteLine("Veeretasin nr" + number);
+        break;
+
+    case 6:
+        Console.WriteLine("Sisestasin nr" + number);
+        break;
+
+    default:
+        Console.WriteLine("Error");
+        break;
+}
