cd457fc73e0a7e0dafb57cf23c2611068fe58ffe
K24101855KristoMihkelson <krismihkel@gmail.com>
2025-04-29 18:15:40 +0300

    Add files via upload

---
+++ any work too hard.py
@@ -0,0 +1,4 @@
+tööriistad = ["haamer","ämber"]
+uustööriist = input("Lisa juurde tööriist:")
+tööriistad.append(uustööriist)
+print(tööriistad)
--- kodutöö.py
+++ kodutöö.py
@@ -1,74 +1,77 @@
-#1 – püstine müügiautomaat
-
-# kirjutage programm, mis:
-
-# - küsib kasutajalt, kas ta soovib kuulda anekdooti?
-
-# - kui jah, siis küsib kasutajalt, kas talle meeldib:
-
-# - - rahvused
-
-# - - linnud-loomad
-
-# - - või sõda
-
-# - kui kasutaja valib rahvuse, esitatakse kasutajale juhuslik arv 1 kolmest rahvusega seotud anekdoodist
-
-# - kui kasutaja valib linnud-loomad, esitatakse kasutajale juhuslik arv 1 kolmest lindude ja loomadega seotud anekdoodist
-
-# - kui kasutaja valib sõja, esitatakse kasutajale juhuslik arv 1 kolmest sõjaga seotud anekdoodist.
-
-# - kui kasutaja otsustab anekdooti mitte tahta, hakkab programm küsima
-
-# NB - anekdoodid tuleb avaldada nii, et anekdoodis endas kasutatakse jutumärke
-
+# - küsib kasutajalt kas ta otsib mingit seent või marja
+# - küsimine toimub tsükli sees
+# - - kui seent, siis programm esitab talle seente järjendi sisu. (seal on seened mida te panna otsustasite)
+# - - - siis küsib kasutajalt millise seene kohta ta lugeda tahab, ning teisest - sisujärjendist - kuvatakse
+# kasutajale seene kohta tekst
+# - - - - Kasutajale antakse koos seeneloendiga ka üks alati esinev valikuvõimalus - "lisa juurde"
+# Lisa juurde valikuga lisatakse seentejärjendile otsa uus element, mille sisend küsitakse kasutajalt
+# - - - - sama tehakse ka sisujärjendiga kuhu küsitakse otsa uue sisestatud seene juurde ka selle seene kirjelduslikm info.
+# - - kui marja, siis programm esitab talle marjade järjendi sisu. (seal on marjad mida te panna otsustasite)
+# - - - siis küsib kasutajalt millise marja kohta ta lugeda tahab, ning teisest - sisujärjendist - kuvatakse
+# kasutajale marja kohta tekst
+# - - - - Kasutajale antakse koos marjaloendiga ka üks alati esinev valikuvõimalus - "lisa juurde"
+# Lisa juurde valikuga lisatakse marjajärjendile otsa uus element, mille sisend küsitakse kasutajalt
+# - - - - sama tehakse ka sisujärjendiga kuhu küsitakse otsa uue sisestatud marja juurde ka selle marja kirjelduslikm info.
+# - pärast seda küsib programm uuesti kas ta tahab lugeda veel millegi kohta, ning programm läheb
 from random import randint
+from tkinter import*
 
-anekdoot = ""
-anekdoot = input("Sa tahad kuulda anekdooti: (jah/ei)")
-if anekdoot == "jah":
-    rahvused = ""
-    while(rahvused == ""):
-        rahvused = input("Sisesta rahvustega seotud anektoodid")
-        suvaline_arv = randint(1,3)
-        if suvaline_arv == 1 :
-            print(" 'Karu hüppas vetsupoti peale' ")
-        elif suvaline_arv == 2:
-            print = (" 'Tort visati inimesele näkku' ")
-        elif suvaline_arv == 3:
-            print = (" 'Vanamees sai suure kulbi' ")
-    print(suvaline_arv)
-elif anekdoot == "jah":
-    LiLo = ""
-        while(LiLo = ""):
-            LiLo = input("'Sisesta lindude ja loomade anekdooti'")
-            suvaline_arv = randint(1,3)
-            if suvaline_arv == 1:
-                print(" 'Lind tegi enda asju sinu pea peale' ")
-            elif suvaline_arv == 2:
-                print("'Su pea on nagu linnu pesa'")
-            elif suvaline_arv == 3:
-                print("'Karuott tõstis enda päkad ülesse'")
-        print(suvaline_arv)
-elif anekdoot == "jah":
-    sõda = ""
-    while(sõda = ""):
-        sõda = input("Sisesta sõja anekdoodid:")
-        suvaline_arv = randint(1,3)
-        if suvaline_arv == 1:
-            print("'Bomb maandus sinna kuhu kedagi ei oodatud'")
-        elif suvaline_arv ==2:
-            print("'ise ei tapnud vaenlast vaid iseennast'")
-        elif suvaline_arv ==3:
-            print("'Vaenlane oli lollim ja andis enda tarkuse mulle edasi'")
-    print(suvaline_arv)
+seened = ["puravik","kukeseen","kukeseen"]
+
+seente_kirjeldused = ["Puravik on paksu jalaga söögiseen.","Kukeseen on kollane ja väga maitsev seen.","Kärbseseen on mürgine ja punase mütsiga seen."]
+
+
+marjad = ["maasikas","mustsõstar","jõhvikas"]
+marjade_kirjeldused = ["Maasikas on magus ja punane mari, mida armastatakse suvel süüa."
+                       ,"Jõhvikas on hapukas punane mari, mida kasutatakse sageli mahlades ja moosides."
+                       ,"Mustsõstar on tumeda värvusega mari, millel on tugev ja aromaatne maitse."]
+valik = ""
+while(valik == ""):
+    valik = input("Kas sa otsid mingit seent või marja:").lower()
+    if valik == "seent":
+        hetkesisestus = 0
+        while(hetkesisestus == 0):
+            hetkesisestus = int(input("Vali enda jaoks sobivaimaid seened " + str(seened) + " ,vali enda jaoks sobiv arv,vajuta enter"))
+            if hetkesisestus != 0:
+                print(seente_kirjeldused[hetkesisestus -1])
+                
+                
+           
+                
+           
+    
+    elif valik == "marja":
+            marja = []
+            marjade_kirjeldused = ()
+            hetkesisestus = "a"
+            while(hetkesisestus != ""):
+                hetkesisestus = input("Sisesta enda jaoks sobivaimaid marju,kui rohkem ei ole,siis vajuta enter")
+                if hetkesisestus != "":
+                    kirjeldus = ""
+                    kirjeldus= input("Kirjelda konkreetset marja " + str(hetkesisestus) + ":")
+                    marja.append(hetkesisestus)
+                    marjade_kirjeldused.append(kirjeldus)
+                print("\nSinu valitud marjad:")
+                for mari in marja:
+                    print("" + str(mari) + " : " + str(marjade_kirjeldused)+ " " + str(marja) + " ")        
 else:
-    ei_taha_anekdooti = ""
-    while( ei_taha_anekdooti = ""):
-        ei_taha_anekdooti = input("Sa tahad anekdooti? (jah/ei)")
-        if ei_taha_anekdooti == "jah":
-            print("head aega")
-        elif ei_taha_anekdooti == "ei":
-            print("Mine tagasi algusesse")
-            
+    SaTahadVeelMidagiVeelUurida = ""
+    SaTahadVeelMidagiVeelUurida = input("Kas sa tahad veel midagi lugeda: (jah/ei/lisa)").lower()
+    if SaTahadVeelMidagiVeelUurida == "jah":
+        print("Mine internetti ja otsi maasikate või puravikude kohta infot juurde").lower()
+    elif SaTahadVeelMidagiVeelUurida == "ei":
+        raam = Tk()
+        raam.title("marja ots")
+        tahvel = Canvas(raam,width = 700, height = 700)
+        tahvel.create_line(387,289,387,130,width = 5, fill = "brown")
+        tahvel.create_polygon(387,289,393,277,440,221,520,206,541,220,width = 5,fill = "brown",outline = "black")
+        tahvel.create_polygon(387,289,282,271,348,233,299,211,289,222,267,235,width = 5,fill = "brown",outline = "black")
+        tahvel.create_polygon(387,289,382,249, 366,197,341,157, 281,156,width = 5,fill = "brown",outline = "black")
+        tahvel.create_polygon(387,289,393,260,430,188,472,150,518,144,width = 5,fill = "brown",outline = "black")
+        tahvel.pack()
+        raam.mainloop()
+    elif SaTahadVeelMidagiVeelUurida == "lisa":
+
+ 
+         
     
